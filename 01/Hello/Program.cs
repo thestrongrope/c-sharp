1da547b25e8277a02d855f49f2a78baee8deec42
Asif Shakir <asif.shakir@gmail.com>
2022-06-26 15:41:31 +0530

    Session No 2

---
+++ 01/Hello/Program.cs
@@ -0,0 +1,14 @@
+﻿Math m = new Math();
+int total = 0;
+int x = (new Random()).Next(10, 20);
+int y = (new Random()).Next(10, 20);
+total = m.add(x, y);
+
+Console.WriteLine($"The sum of {x} and {y} is {total}");
+Console.WriteLine($"The concat of {x} and {y} is {m.add(x.ToString(), y.ToString())}");
+
+public class Math {
+    public int add(int x, int y) => x + y;
+    public string add(string x, string y) => x + y;
+    public float add(float x, float y) => x + y;
+}
---
+++ 02/Grading/Program.cs
@@ -0,0 +1,39 @@
+﻿// What is grade
+// Grade = Marks
+// If Next Multiple of 5 of Marks - Grade < 3 grade = next Multiple of 5
+// 11 / 5 = 2 * 5 = 10 => 15
+// 67 / 5 = 13 * 5 = 65 => 70
+// 9 / 5 = 1 * 5 = 5 => 10
+// generating a random number between 0 and 100
+// int marks = (new Random()).Next(0, 100);
+
+int[] studentMarks = { 35, 45, 84, 29, 57, 73, 67, 38, 33};
+
+List<int> studentGrades = studentMarks.ToList();
+List<int> newGrades = new List<int>();
+foreach(int marks in studentGrades) {
+    newGrades.Add(getGrade(marks));
+}
+
+for(int i = 0; i < studentGrades.Count; i++) {
+    int marks = studentGrades[i];
+    int grade = newGrades[i];
+    Console.WriteLine($"Marks: {marks}. Grade: {grade}");
+}
+
+int getGrade(int marks) {
+
+    if(marks < 38) return marks;
+
+    int grade;
+    grade = marks;
+
+    int nextMultipleOf5;
+    nextMultipleOf5 = ( ( marks / 5 ) * 5 ) + 5;
+
+    int difference = nextMultipleOf5 - grade;
+
+    if ( difference < 3 ) grade = nextMultipleOf5;
+
+    return grade;
+}
