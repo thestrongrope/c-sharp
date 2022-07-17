ef338d3c225a96bc3872a93207c807e13ec15091
Asif Shakir <asif.shakir@gmail.com>
2022-07-17 15:58:10 +0530

    oops concepts

---
+++ 04/oops/Program.cs
@@ -0,0 +1,106 @@
+﻿public class Program {
+    public static void Main() {
+        Car c = new Car();
+        c.Wheels = 10;
+        c.isRunning = false;
+    }
+} 
+
+/* 
+    Class is a collection of variables and functions
+
+    A field is a variable defined within a class
+        fields should always be private
+        fields should always be in camelCase
+        fields may start with an underscore
+
+        what does it mean if a field is static?
+
+            What is the difference between a Class and an Object?
+                -> A class is a definition / blueprint
+                    -> a class occupies memory
+                        -> functions
+                        -> static variables
+
+                -> An object is an instance of a class 
+                    -> an object occupies memory
+                        -> variables
+
+    A property is a variable defined within a class
+        It is a field it is back by a getter function and a setter function
+        Should always be public
+        Should always be PascalCase
+        Will always have a getter and a setter
+
+    A method is a function defined with a class
+*/
+
+internal class BankAccount {
+
+    private float accountBalance = 0;
+    public float AccountBalance {
+        get {
+            //Notifier.NotifyAccountHolder();
+            return accountBalance;
+        }
+        set {
+            accountBalance = value;
+        }
+    } 
+}
+
+/*
+
+    What is the difference between a Class and an Object?
+        -> A class is a definition / blueprint
+            -> a class occupies memory
+                -> functions
+                -> static variables
+
+        -> An object is an instance of a class 
+            -> an object occupies memory
+                -> variables
+
+    Car.IsCoupe(); // static call
+
+    Static Functions / Static Classes
+    Library 
+
+    static Math Library - has no fields or properties
+        static Add(int a, int b)
+        static Log(int a)
+        static Ln(int a)
+        static Sin(int a)
+    
+    (new Car()).IsCoupe(); // non-static call
+
+*/
+
+internal class Car {
+    /* Fields */
+    private bool isRunning = false;
+    /* Properties */
+    public int Wheels {get; set;}
+    public int Doors {get; set;}
+    /* Methods */
+    public bool IsCoupe() => Doors == 2;
+    public bool IsRunning() => isRunning;
+}
+
+// Serializing a class
+// Converting a memory object of a class to a file
+
+// access specifier
+// public, private, internal, protected
+
+internal class MercedesBenz : Car {
+    public new int Doors { get => 4; private set {} }
+    public bool FoldableRoof { get; set; }
+    public bool LifeInsurance {get;set;}
+    public Chair CarChair {get; set;}
+}
+
+public class Chair {
+    public int Quality {get;set;}
+    public bool IsHighQuality => Quality == 5;
+}
---
+++ 04/questions.md
@@ -0,0 +1,35 @@
+Questions
+=========
+* OOPs => Object Oriented Programming System
+* C++, Java, C#
+
+// Simple Imperative
+int a = 1;
+print("Hello World");
+
+// Imperative coding
+// branching => conditional statements (if, ternary operator)
+// looping
+
+* What is a function? Imperative or Functional Programming
+    * It is a collection of reusable statements.
+
+* What is a class?
+    * It is a collection of variables and functions
+    * A variable in a class is called a field or a property
+    * A function in a class is called a class Method
+
+* What are the four pillars of OOPs?
+    * Inheritance
+    * Encapsulation
+    * Abstraction
+        * For a class
+    * Polyphormism
+        * For methods of a class
+            * Run-time
+            * Compile-time
+
+* What is SOLID?
+    * S O L I D
+    
+==> 2 yrs of exp.
