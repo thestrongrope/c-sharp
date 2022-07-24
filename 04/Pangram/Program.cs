1e6fadaa36d3e267b0d156e387a774abbd1014d7
Asif Shakir <asif.shakir@gmail.com>
2022-07-24 14:32:00 +0530

    Pangram

---
+++ 04/Pangram/Program.cs
@@ -0,0 +1,23 @@
+﻿string sentence = "The quick brown fox jumps over the lazy dog";
+string[] words = sentence.Split(' ');
+List<string> reversedWords = new();
+foreach(string word in words) {
+    string reversed = ReverseString(word);
+    reversedWords.Add(reversed);
+}
+string pangramString = string.Join(' ', reversedWords);
+Console.WriteLine($"Using List: {pangramString}");
+
+string[] reversedWordsArray = new string[words.Length];
+for(int i = 0; i < words.Length; i++) {
+    string reversed = ReverseString(words[i]);
+    reversedWordsArray[i] = reversed;
+}
+pangramString = string.Join(' ', reversedWordsArray);
+Console.WriteLine("Using Arrays: {0}", pangramString);
+
+string ReverseString(string str) {
+    var characters = str.ToCharArray();
+    Array.Reverse(characters);
+    return new string(characters);
+}
